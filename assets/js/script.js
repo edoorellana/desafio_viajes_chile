417900260513d09305c8c39fc4a2a29bbf9846a5
edoorellana <ed.guillermo.orellana@gmail.com>
2024-07-27 22:12:16 -0400

    Se agregaron alertas para secciones quienes somos, destacados y contacto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $('#quienes-somos').click(function(){
+        alert("Somos la mejor empresa de viajes del País")
+    });
+
+    $('#destacados').click(function(){
+        alert("Comparte en los mejores lugares de Chile y el Mundo")
+    });
+    
+    $('#contacto').click(function(){
+        alert("Que esperas para vivir una nueva experiencia?")
+    });
+ 
+  })
+
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
